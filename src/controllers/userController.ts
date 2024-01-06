4589f3de24aad0bf003050abb881983c3c19e6a2
CHIAMCK <chiamck91@hotmail.com>
2024-01-06 10:39:33 +0800

    feat: implement logic to hash password

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,21 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
 import UserModel from '../models/user';
+import * as bcrypt from 'bcrypt';
 
 class UserController {
   async createUser(req: Request, res: Response, next: NextFunction) {
     try {
       const { firstName, lastName, email, password } = req.body;
-
+      const bcryptRounds = 10;
+      const hashedPassword = await bcrypt.hash(password, bcryptRounds);
       // Validate request data (you may want to use a validation library)
       const newUser = new UserModel({
         firstName,
         lastName,
         email,
-        password,
+        password: hashedPassword,
       });
 
-      const savedUser = await newUser.save();
-      res.status(201).json(savedUser);
+      res.status(201).json();
     } catch (error) {
       next(error);
     }
